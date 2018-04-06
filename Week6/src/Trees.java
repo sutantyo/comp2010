1469a85a62c818a7997d8800fcca6350f4dce344
Daniel Sutantyo <daniel.sutantyo@gmail.com>
2018-04-06 10:57:06 +1000

    added a few more codes

--- Week6/src/Trees.java
+++ Week6/src/Trees.java
@@ -13,6 +13,9 @@ public class Trees {
 		test.insert(1);
 		test.insert(3);
 		
+		print(test.root);
+		System.out.println();
+		/*
 		System.out.println(test.inorder());
 		System.out.println(test.postorder());
 		
@@ -21,12 +24,24 @@ public class Trees {
 		System.out.println(sumNodes(test.root));
 		printBetween(test.root,3,6);
 		System.out.println();
+		*/
 	}
 	
 	public static int countNodes(Node root){
 		if (root == null)
 			return 0;
 		return 1 + countNodes(root.left) + countNodes(root.right);
+	}
+	
+	public static int countLeaves(Node root){
+		if (root.left == null && root.right == null)
+			return 1;
+		else if (root.left == null)
+			return countLeaves(root.right);
+		else if (root.right == null)
+			return countLeaves(root.left);
+		else
+			return countLeaves(root.left) + countLeaves(root.right);
 	}
 	
 	public static int findMax(Node root){
@@ -38,7 +53,7 @@ public class Trees {
 	public static int sumNodes(Node root){
 		if (root == null)
 			return 0;
-		return sumNodes(root.left) + sumNodes(root.right) + root.key; 
+		return root.key + sumNodes(root.left) + sumNodes(root.right);
 	}
 	
 	public static void printBetween(Node root, int a, int b){
@@ -48,6 +63,22 @@ public class Trees {
 			System.out.print(root.key + " ");
 		printBetween(root.left,a,b);
 		printBetween(root.right,a,b);
+	}
+	
+	public static int findHeight(Node root){
+		if (root == null)
+			return 0;
+		else
+			return 1 + Math.max(findHeight(root.left), findHeight(root.right));
+	}
+	
+	public static void print(Node root){
+		if (root == null)
+			return;
+		print(root.right);
+		print(root.left);
+		System.out.print(root.key + " ");
+		
 	}
 
 }
