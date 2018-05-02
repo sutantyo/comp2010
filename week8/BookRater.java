da6988c8ff00be09669cd8768c7da5ee90ce8c08
sutantyo <daniel.sutantyo@gmail.com>
2018-05-02 13:21:44 +1000

    Delete BookRater.java

--- week8/BookRater.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class BookRater implements Comparator<Book> {
-
-    public int compare(Book b1, Book b2){
-        return b1.getRating().compareTo(b2.getRating());
-    }
-}
